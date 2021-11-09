1dbdb7109e53d962f863903189482f29c82a094b
Rafael <rafaelsfsoares@gmail.com>
2021-11-09 19:47:30 -0300

    FEAT: get tasks route

--- index.js
+++ index.js
@@ -7,18 +7,21 @@ app.get('/', (req, res) => {
 
 })
 
-app.get('/usuarios', (req, res) => {
-  res.send('Rota ativada com GET e recurso usuario: valores de usuario devem ser retornados')
 
-})
+  function usuarios(req, res) {
+      res.send('Rota ativada com GET e recurso usuario: valores de usuario devem ser retornados')
+  }
 
-app.get('/tarefas', (req, res) => {
-    res.send('Rota ativada com GET e recurso tarefas: valores de tarefas devem ser retornados')
   
-  })
+  function tarefas(req, res) {
+    res.send('Rota ativada com GET e recurso usuario: valores de usuario devem ser retornados')
+}
 
+app.get("/usuarios", usuarios)
+app.get("/tarefas", tarefas)
 
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
