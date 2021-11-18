b4aeb6c157f83ced8303aa84723383ce94ef4c70
Rafael <rafaelsfsoares@gmail.com>
2021-11-18 18:58:23 -0300

    FEAT: ADD REMOVE/UPDATE

--- src/Controllers/TaskControllers/TaskControllers.js
+++ src/Controllers/TaskControllers/TaskControllers.js
@@ -1,11 +1,128 @@
+const taskRequire = require ("../../Models/tasksmodels");
+
+const { tasksDB } = require ("../../infra/bd");
+
+const TasksModel = require('../../Models/tasksmodels');
+
+const {uuid} = require ("uuidv4");
+
 class TaskControllers {
-    show =(req, res) => {
-    res.send('Rota ativada com GET e recurso tarefa: valores de tarefa devem ser retornados')
-}
-    save = (req, res) => {
-    res.send ("testando post tasks")
+    constructor(dbConn) {
+        this.dbConn = dbConn;
+    }
+
+    show = (req, res) => {
+        const titulo = req.params.titulo
+
+        this.dbConn.forEach((task) =>{
+            if(task.titulo === titulo){
+                res.send(task);
+            }
+        });
+    };   
+
+    index = (req, res) =>{
+        res.send(this.dbConn);
+    };
+
+        save = (req, res) => {
+            const {titulo, descricao, status} = req.body;
+
+            const tasks = new TasksModel(titulo, descricao, status);
+
+            // salva no banco
+            this.dbConn.push(tasks);
+
+            
+            res.send(tasks)
+    };
+
+
+    remove = (req, res) => {
+        const titles = req.params.titulo
+
+        this.dbConn = this.dbConn.filter((t) =>{
+         t.titulo === titulo;  
+        });
+
+        const index = this.dbConn.indexOf(task[0]);
+
+        this.dbconn.splice(index, 1);
+
+        console.log(this.dbConn);
+
+        res.send("teste");
+        
+        
+    }
+
+
+    update = (req, res) => {
+        const titulo = req.params.titulo;
+        const content = req.body;
+
+
+        for (let i = 0; i < this.dbConn.length; i++){
+            if ((this.dbConn[i].titulo = titulo)){
+                this.dbConn[i] = content
+            }
+        }
+
+
+        // this.dbConn.forEach((task) =>{
+        //     if(task.titulo === titulo){
+        //         task = content;
+        //     }
+        // });
+
+            res.send(`tasks:${titulo} porta sendo utilizada`)
+
+    }
 };
-}
 
 
-module.exports = TaskControllers;
+    
+    
+module.exports = new TaskControllers(tasksDB);
+
+
+
+
+
+
+
+
+
+
+// class TaskControllers {
+//     constructor(dbConn){
+        
+//         this.dbConn = dbConn;
+//     }
+    
+    // show =(req, res) => {
+
+    // this.dbConn.forEach(task =>{
+    //     if(task.titulo === req.params.titulo){
+    //         res.send(task);
+    //     }
+    // })
+
+//     console.log(req.params.titulo);
+
+//     const taskShow = new taskRequire("Tibuço", "amigão", "ativo", "16/11/2021")    
+
+//     res.send(taskShow)
+// }
+//     save = (req, res) => {
+
+//     const taskSave = new taskRequire(uuid(),req.body.titulo, req.body.descricao, req.body.status, req.body.dataDeCriacao)  
+
+//     this.dbConn.push(taskSave) 
+
+//     res.send (taskSave);
+// };
+// }
+
+
+module.exports = new TaskControllers(tasksDB);
--- src/Controllers/UsersControllers/UsersControllers.js
+++ src/Controllers/UsersControllers/UsersControllers.js
@@ -1,13 +1,79 @@
+const usersRequire = require ("../../Models/usersmodels");
+
+const { userDB } = require ("../../infra/bd")
+
+
+const {uuid} = require ("uuidv4")
+
 class UsersControllers {
+    constructor(dbConn){
+        this.dbConn = dbConn;
+    }
+
     show = (req, res) => {
-    res.send('Rota ativada com GET e recurso usuario: valores de usuario devem ser retornados')
-}
-    save = (req, res) => {
-        res.send ("testando post users")
+        const nome = req.params.nome;
+    
+        this.dbConn.forEach((user) => {
+          if (user.nome === nome) {
+            res.send(user);
+          };
+        });
+      };
+
+    index = (req, res) =>{
+    res.send(this.dbConn);
     };
+
+
+    save = (req, res) => {
+        const { nome, email, senha } = req.body;
+    
+        const users = new UsersModel(nome, email, senha);
+    
+        // salva no banco
+        this.dbConn.push(users);
+    
+        res.send(users);
+      };
+
+    
+
+
+    remove = (req, res) => {
+
+        const { nome } = req.params
+
+        this.dbConn = this.dbConn.filter((user) =>{
+         user.nome === nome;  
+        });
+
+        const index = this.dbConn.indexOf(user[0]);
+
+        this.dbconn.splice(index, 1);
+
+        console.log(this.dbConn);
+
+        res.send("teste");
+        
+        
+    }
+
+    
 }
 
 
+module.exports = new UsersControllers(userDB);
 
 
-module.exports = UsersControllers;
+
+
+// save = (req, res) => {
+
+    //     const userSave = new usersRequire(uuid(),req.body.nome,req.body.email,req.body.senha)
+
+    //     console.log(req.body) //lembrar de colocar o console.log
+
+    //     this.dbConn.push(userSave) 
+
+    //     res.send (userSave)
+    // }; exemplo feito na breakout com o grupo
