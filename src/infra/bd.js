c785fad72ec2c1195df207dcc3b781a3d5acdc3e
Rafael <rafaelsfsoares@gmail.com>
2021-11-18 19:00:49 -0300

    CREATE INFRA AND CONFIG FILES

---
+++ src/infra/bd.js
@@ -0,0 +1,11 @@
+module.exports = {
+    userDB: [],
+    tasksDB: [],
+}
+    //utiliza estruturas em arrays para acrescentar dados no banco.
+
+
+
+    //userDB.push() ~> adicionar um item a lista
+    //userDB.pop() apaga o último da lista
+    //user
